16f81eab5bde499af60fbfd9e1d81aecfd5beedd
fazlerabbi13 <mohammad0132794@gmail.com>
2026-01-22 18:49:55 +0600

    define a class and function.and generate random numbers.

---
+++ GuessingGame.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void guessingNumber(){
+        Scanner sc  = new Scanner(System.in);
+
+        int number = 1 + (int)(100 * Math.random());
+
+        int k = 5;
+    }
+}
